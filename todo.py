e1c98d3ba669d01f1aeac0f9933cd9d423d5913f
Yael Solan <yaelsolan5@gmail.com>
2022-12-24 13:34:10 +0200

    new routes and files

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 import sqlite3
-from bottle import route, run, debug, template
+from bottle import route, run, debug, template, request, static_file
 
 @route('/todo')
 def todo_list():
@@ -11,6 +11,82 @@ def todo_list():
     output = template ('make_table', rows=result)
     return output
     return str(result)
+    return template('make_table', rows=result)
+    
+@route('/new', method='GET')
+def new_item():
+
+    new = request.GET.task.strip()
+
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new, 1))
+    new_id = c.lastrowid
+
+    conn.commit()
+    c.close()
+
+    return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id 
+ 
+    if request.GET.save:
+
+        new = request.GET.task.strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+        new_id = c.lastrowid
+
+        conn.commit()
+        c.close()
+
+        return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
+    else:
+        return template('new_task.tpl')
+        
+@route('/edit/<no:int>', method='GET')
+def edit_item(no):
+
+    if request.GET.save:
+        edit = request.GET.task.strip()
+        status = request.GET.status.strip()
+
+        if status == 'open':
+            status = 1
+        else:
+            status = 0
+
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (edit, status, no))
+        conn.commit()
+
+        return '<p>The item number %s was successfully updated</p>' % no
+    else:
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no),))
+        cur_data = c.fetchone()
+
+        return template('edit_task', old=cur_data, no=no)    
+        
+@route('/item<item:re:[0-9]+>')
+def show_item(item):
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("SELECT task FROM todo WHERE id LIKE ?", (item,))
+    result = c.fetchall()
+    c.close()
+    if not result:
+        return 'This item number does not exist!'
+    else:
+        return 'Task: %s' % result[0] 
+         
+@route('/help')
+def help():
+    return static_file('help.html', root='/path/to/file')        
+           
 debug(True)
 run(reloader=True)
 
